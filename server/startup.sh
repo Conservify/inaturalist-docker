4c2ffacb7950e3df69dc44d4ad443f44698212f5
Jacob Lewallen <jlewallen@gmail.com>
2018-04-13 11:10:06 -0700

    Dummy data.

--- server/startup.sh
+++ server/startup.sh
@@ -11,17 +11,16 @@ if [ ! -f database-ready ]; then
 
     rake es:rebuild
 
-    rails r tools/load_sources.rb 
+    rails r tools/load_sources.rb
     rails r tools/load_iconic_taxa.rb
     rails r "Site.create( name: 'iNaturalist', url: 'http://127.0.0.1:3000' )"
-    rails r tools/import_natural_earth_countries.rb
+    # This fails with a locale conversion error, for me.
+    # rails r tools/import_natural_earth_countries.rb
     rails r tools/import_us_states.rb
+    rails r tools/import_us_counties.rb
 
     rails r "User.create( login: 'testperson', password: 'test', password_confirmation: 'test', email: 'test@test.com' )"
-    # rails r tools/import_natural_earth_countries.rb
-    # rails r tools/import_us_states.rb
-    # rails r tools/import_us_counties.rb
-    # rails r tools/load_dummy_observations.rb
+    rails r tools/load_dummy_observations.rb
 
     touch database-ready
 fi

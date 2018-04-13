7149846d4b4f0c801a5f0bc0e4a54bb8d8308342
Jacob Lewallen <jlewallen@gmail.com>
2018-04-13 12:14:33 -0700

    Scripts for shelling into things. Create test users.

---
+++ db-shell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -it inaturalistdocker_postgres_1 psql -U in inaturalist_development
--- server/startup.sh
+++ server/startup.sh
@@ -14,13 +14,15 @@ if [ ! -f database-ready ]; then
     rails r tools/load_sources.rb
     rails r tools/load_iconic_taxa.rb
     rails r "Site.create( name: 'iNaturalist', url: 'http://127.0.0.1:3000' )"
-    # This fails with a locale conversion error, for me.
-    # rails r tools/import_natural_earth_countries.rb
     rails r tools/import_us_states.rb
-    rails r tools/import_us_counties.rb
 
-    rails r "User.create( login: 'testperson', password: 'test', password_confirmation: 'test', email: 'test@test.com' )"
+    rails r "User.create!( login: 'jacobtest', password: 'asdfasdf', password_confirmation: 'asdfasdf', email: 'jacobtest@test.com', name: 'Jacob' )"
+    rails r "User.create!( login: 'shahtest', password: 'asdfasdf', password_confirmation: 'asdfasdf', email: 'shahtest@test.com', name: 'Shah' )"
     rails r tools/load_dummy_observations.rb
+
+    # These fails with a locale conversion errors.
+    # rails r tools/import_natural_earth_countries.rb
+    # rails r tools/import_us_counties.rb
 
     touch database-ready
 fi

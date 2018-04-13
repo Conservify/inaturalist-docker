f8d80863b5fc40882af5ccbd426073902292fea0
Jacob Lewallen <jlewallen@gmail.com>
2018-04-13 08:40:23 -0700

    Get some seed data in.

--- server/startup.sh
+++ server/startup.sh
@@ -5,13 +5,21 @@ set -xe
 if [ ! -f database-ready ]; then
     rake db:setup
 
-    rails r "User.create( login: 'testerson', password: 'test', password_confirmation: 'test', email: 'test@test.com' )"
+    rake es:rebuild
+
+    rails r tools/load_sources.rb 
+    rails r tools/load_iconic_taxa.rb
+    rails r "Site.create( name: 'iNaturalist', url: 'http://127.0.0.1:3000' )"
     rails r tools/import_natural_earth_countries.rb
     rails r tools/import_us_states.rb
-    rails r tools/import_us_counties.rb
-    rails r tools/load_dummy_observations.rb
+
+    rails r "User.create( login: 'testperson', password: 'test', password_confirmation: 'test', email: 'test@test.com' )"
+    # rails r tools/import_natural_earth_countries.rb
+    # rails r tools/import_us_states.rb
+    # rails r tools/import_us_counties.rb
+    # rails r tools/load_dummy_observations.rb
 
     touch database-ready
 fi
 
-rails s
+rails s -b 0.0.0.0
